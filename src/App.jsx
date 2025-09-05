844ff868fd8d209c208ae8db6a6b9d3b0192b712
Brixtongrey <brixtonaruda@gmail.com>
2025-09-05 16:13:04 -0600

    feat: created EpisodeDetails function

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,48 @@
+import { episodeList } from "./data";
+
 function App() {
  
   const [episodes] = useState(null);
+  const [selectedEpisode, setSelectedEpisode] = useState();
 
 
+  function EpisodeDetails() {
+    if(!selectedEpisode) {
+      return (
+        <>
+        <section className="details">
+        <h2>Episode</h2>
+        <p>Select an episode to learn more.</p>
+        </section>
+        </>
+      );
+    }
+  }
+
+  return (
+    <>
+    <section className="details">
+    <h2>Episode: {selectedEpisode.id}</h2>
+    <h3>{selectedEpisode.title}</h3>
+    <p>{selectedEpisode.description}</p>
+    <button>Watch Now</button>
+    </section>
+    </>
+  );
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
 export default App;
+
